dd9deb765e35ca40a8a51cc188ff43c7a49907fa
DubJ <jeremy.ah33@gmail.com>
2022-02-11 14:13:17 +0100

    model user + signUp

---
+++ controllers/auth.controller.js
@@ -0,0 +1,19 @@
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+
+const userModel = require('../models/user.model');
+const User = require('../models/user.model');
+
+module.exports.signUp = async (req, res) => {
+    const {firstName, lastName, email, password} = req.body;
+
+    try 
+    {
+        const user = await userModel.create({firstName, lastName, email, password });
+        res.status(201).json({ user: user._id });
+    }
+    catch(err)
+    {
+        res.status(200).send({ err });
+    }
+};
--- models/user.model.js
+++ models/user.model.js
@@ -1,36 +1,49 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = mongoose.Schema({
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true,
-        trim: true
-     },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        max: 1024,
-        minLength: 6
-    },
-    adress:
+const userSchema = mongoose.Schema(
     {
-        type: String,
-        required: false,
-        trim: true
-    },    
-  });
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: { 
+            type: String, 
+            required: true, 
+            unique: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            max: 1024,
+            minLength: 6
+        },
+        adress:
+        {
+            type: String,
+            required: false,
+            trim: true
+        },
+        phoneNb:
+        {
+            type: Number,
+            min: [10, "Le numéro de téléphone n'est pas valide"],
+            max: 10,
+            required: false,
+            trim: true
+        },
+    },
+    { 
+        timestamps: true,
+    }
+  );
   
   userSchema.plugin(uniqueValidator);
   
